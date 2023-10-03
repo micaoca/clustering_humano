1731d0f3f2f4fe9f89080924b950686f2ba8c33a
Luca <luca-diaz@live.com>
2023-10-03 15:52:47 -0300

    se agrega funcionalidad y vista

--- src/clustering_humano/Aplicacion.java
+++ src/clustering_humano/Aplicacion.java
@@ -1,71 +1,30 @@
 package clustering_humano;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Aplicacion {
 	
-	public HashMap<Integer, Persona> personas = new HashMap<>();
-	private Grafo grafo;
-	private int contador;
+	public ArrayList<Persona> personas = new ArrayList<>();
+	private GrafoPersonas grafo;
 
-	public Aplicacion() {
-		contador = 0;
+	public Aplicacion() {}
+
+	public void agregarPersona(String nombre, int d, int m, int e, int c) {
+		personas.add(new Persona(nombre,d,m,e,c));
 	}
-
-	 public void agregarPersona(String nombre, int d, int m, int e, int c) {
-		 	Persona persona = new Persona(nombre,d,m,e,c);
-	        personas.put(contador, persona);
-	        contador += 1;
-	 }
 	 
-	 
-	 public void armarGrupoPersonas() {
-		 armarGrafo();
-		 agregarAristas();
-	 }
-	 
-	 private void armarGrafo() {
-		 
-		 if(personas.size() < 0 || personas == null) {
-			 throw new NullPointerException("La lista es nula");
-		 }
-		 
-		 this.grafo = new Grafo(personas.size());
-		
-	 }
-	 
-	 private void agregarAristas() {
-		  
-		for(int fila = 0; fila < personas.size() ; fila++) {
-			for(int columna = (fila + 1); columna < personas.size() ; columna++) {
-				grafo.agregarArista(fila, columna, indiceSimiliridad(fila, columna));
-			}	
+	public void armarGrupoPersonas() throws Exception {
+		try {
+			armarGrafo();
+			grafo.armarAristas();
+		} catch(Exception e) { 
+			throw new Exception("Error interno.");
 		}
-
-		this.grafo.imprimirMatriz();
-		this.grafo.obtenerAristaMenorPeso();
-		
-	 }
-	
-	 private int indiceSimiliridad(int fila, int columna) {
-		 
-		 //intereses persona 1
-		 int d1 = personas.get(fila).getD();
-		 int m1 = personas.get(fila).getM();
-		 int e1 = personas.get(fila).getE();
-		 int c1 = personas.get(fila).getC();
-		 
-		 //intereses persona 2
-		 int d2 = personas.get(columna).getD();
-		 int m2 = personas.get(columna).getM();
-		 int e2 = personas.get(columna).getE();
-		 int c2 = personas.get(columna).getC();
-		 
-		 int indiceSimilaridad = (d1 - d2) + (m1 -  m2) + (e1 - e2) + (c1 - c2);
-
-		 return indiceSimilaridad;
-		 
-	 }
+	}
+	 
+	private void armarGrafo() {
+		this.grafo = new GrafoPersonas(personas);
+	}
 	
 	
 }
--- src/clustering_humano/AplicacionView.java
+++ src/clustering_humano/AplicacionView.java
@@ -4,8 +4,10 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JSplitPane;
+import javax.swing.JTable;
 import javax.swing.JMenu;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -13,6 +15,14 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import java.awt.Color;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import javax.swing.event.AncestorListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.event.AncestorEvent;
+import javax.swing.AbstractListModel;
+import java.awt.CardLayout;
 
 public class AplicacionView extends JFrame {
 
@@ -22,6 +32,8 @@ public class AplicacionView extends JFrame {
 	private JTextField input_interes_cientifico;
 	private JTextField input_interes_espectaculo;
 	private JTextField input_interes_deportivo;
+	private Aplicacion app;
+	private JPanel panelPersonas_1;
 
 	/**
 	 * Launch the application.
@@ -43,27 +55,88 @@ public class AplicacionView extends JFrame {
 	 * Create the frame.
 	 */
 	public AplicacionView() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 672, 513);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(64, 128, 128));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		this.app = new Aplicacion();
+		initialize();
+	}
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+	private void initialize() {
+		iniciarFrame();
 		
+		JPanel panelAggPersona = iniciarPanelAggPersona();
+		
+		agregarPanelPersonas();
+		agregarLabels(panelAggPersona);
+		agregarInputs(panelAggPersona);
+		agregarBotones(panelAggPersona);
+		
+	}
+
+	private void agregarPanelPersonas() {
+		panelPersonas_1 = new JPanel();
+		panelPersonas_1.setBackground(new Color(128, 128, 128));
+		panelPersonas_1.setBounds(0, 269, 656, 205);
+		contentPane.add(panelPersonas_1);
+	}
+
+	
+	//Metodos de conveniencia
+	private JPanel iniciarPanelAggPersona() {
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(192, 192, 192));
 		panel.setBounds(0, 45, 656, 226);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+		return panel;
+	}
+
+	private void agregarLabels(JPanel panel) {
 		JLabel label_nombre = new JLabel("Nombre");
 		label_nombre.setHorizontalAlignment(SwingConstants.CENTER);
 		label_nombre.setFont(new Font("Tahoma", Font.ITALIC, 14));
 		label_nombre.setBounds(33, 86, 241, 14);
 		panel.add(label_nombre);
 		
+		JLabel label_musica = new JLabel("Interés por la música");
+		label_musica.setHorizontalAlignment(SwingConstants.CENTER);
+		label_musica.setLabelFor(input_interes_musical);
+		label_musica.setFont(new Font("Tahoma", Font.ITALIC, 14));
+		label_musica.setBounds(33, 135, 241, 14);
+		panel.add(label_musica);
+		
+		JLabel label_espectaculo = new JLabel("Interés por las noticias del espectáculo");
+		label_espectaculo.setHorizontalAlignment(SwingConstants.CENTER);
+		label_espectaculo.setLabelFor(input_interes_espectaculo);
+		label_espectaculo.setFont(new Font("Tahoma", Font.ITALIC, 14));
+		label_espectaculo.setBounds(33, 160, 241, 14);
+		panel.add(label_espectaculo);
+		
+		JLabel label_deportes = new JLabel("Interés por los deportes");
+		label_deportes.setHorizontalAlignment(SwingConstants.CENTER);
+		label_deportes.setFont(new Font("Tahoma", Font.ITALIC, 14));
+		label_deportes.setBounds(33, 111, 241, 14);
+		panel.add(label_deportes);
+		
+		JLabel label_ciencia = new JLabel("Interés por la ciencia");
+		label_ciencia.setHorizontalAlignment(SwingConstants.CENTER);
+		label_ciencia.setLabelFor(input_interes_cientifico);
+		label_ciencia.setFont(new Font("Tahoma", Font.ITALIC, 14));
+		label_ciencia.setBounds(33, 185, 241, 14);
+		panel.add(label_ciencia);
+		
+		JLabel lbl_agregar_persona = new JLabel("Agregar persona");
+		lbl_agregar_persona.setBounds(257, 23, 150, 22);
+		panel.add(lbl_agregar_persona);
+		lbl_agregar_persona.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lbl_agregar_persona.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JLabel lbl_titulo_app = new JLabel("Clustering humano");
+		lbl_titulo_app.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
+		lbl_titulo_app.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_titulo_app.setBounds(184, 11, 280, 23);
+		contentPane.add(lbl_titulo_app);
+	}
+
+	private void agregarInputs(JPanel panel) {
 		input_interes_musical = new JTextField();
 		input_interes_musical.setHorizontalAlignment(SwingConstants.CENTER);
 		input_interes_musical.setBounds(284, 134, 187, 20);
@@ -82,53 +155,20 @@ public class AplicacionView extends JFrame {
 		panel.add(input_interes_cientifico);
 		input_interes_cientifico.setColumns(10);
 		
-		JLabel lblIntersDeInteres = new JLabel("Interés por la música");
-		lblIntersDeInteres.setHorizontalAlignment(SwingConstants.CENTER);
-		lblIntersDeInteres.setLabelFor(input_interes_musical);
-		lblIntersDeInteres.setFont(new Font("Tahoma", Font.ITALIC, 14));
-		lblIntersDeInteres.setBounds(33, 135, 241, 14);
-		panel.add(lblIntersDeInteres);
-		
 		input_nombre = new JTextField();
-		label_nombre.setLabelFor(input_nombre);
 		input_nombre.setHorizontalAlignment(SwingConstants.CENTER);
 		input_nombre.setBounds(284, 85, 187, 20);
 		panel.add(input_nombre);
 		input_nombre.setColumns(10);
 		
-		JLabel label_interes_espectaculo = new JLabel("Interés por las noticias del espectáculo");
-		label_interes_espectaculo.setHorizontalAlignment(SwingConstants.CENTER);
-		label_interes_espectaculo.setLabelFor(input_interes_espectaculo);
-		label_interes_espectaculo.setFont(new Font("Tahoma", Font.ITALIC, 14));
-		label_interes_espectaculo.setBounds(33, 160, 241, 14);
-		panel.add(label_interes_espectaculo);
-		
-		JLabel lblIntersDeInteres_1 = new JLabel("Interés por los deportes");
-		lblIntersDeInteres_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblIntersDeInteres_1.setFont(new Font("Tahoma", Font.ITALIC, 14));
-		lblIntersDeInteres_1.setBounds(33, 111, 241, 14);
-		panel.add(lblIntersDeInteres_1);
-		
-		JLabel lblIntersPorLa = new JLabel("Interés por la ciencia");
-		lblIntersPorLa.setHorizontalAlignment(SwingConstants.CENTER);
-		lblIntersPorLa.setLabelFor(input_interes_cientifico);
-		lblIntersPorLa.setFont(new Font("Tahoma", Font.ITALIC, 14));
-		lblIntersPorLa.setBounds(33, 185, 241, 14);
-		panel.add(lblIntersPorLa);
-		
 		input_interes_deportivo = new JTextField();
-		lblIntersDeInteres_1.setLabelFor(input_interes_deportivo);
 		input_interes_deportivo.setHorizontalAlignment(SwingConstants.CENTER);
 		input_interes_deportivo.setBounds(284, 110, 187, 20);
 		panel.add(input_interes_deportivo);
 		input_interes_deportivo.setColumns(10);
-		
-		JLabel lbl_agregar_persona = new JLabel("Agregar persona");
-		lbl_agregar_persona.setBounds(257, 23, 150, 22);
-		panel.add(lbl_agregar_persona);
-		lbl_agregar_persona.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lbl_agregar_persona.setHorizontalAlignment(SwingConstants.CENTER);
-		
+	}
+
+	private void agregarBotones(JPanel panel) {
 		JButton btn_agregar_persona = new JButton("Agregar persona");
 		btn_agregar_persona.setBackground(new Color(0, 255, 64));
 		btn_agregar_persona.setFont(new Font("Tahoma", Font.BOLD, 15));
@@ -139,15 +179,41 @@ public class AplicacionView extends JFrame {
 		btn_ejecutar_algoritmo.setBounds(503, 183, 121, 23);
 		panel.add(btn_ejecutar_algoritmo);
 		
-		JLabel lbl_titulo_app = new JLabel("Clustering humano");
-		lbl_titulo_app.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
-		lbl_titulo_app.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl_titulo_app.setBounds(184, 11, 280, 23);
-		contentPane.add(lbl_titulo_app);
+		agregarListeners(btn_agregar_persona, btn_ejecutar_algoritmo);
+	}
+
+	private void agregarListeners(JButton btn_agregar_persona, JButton btn_ejecutar_algoritmo) {
+		btn_agregar_persona.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String inputNombre = input_nombre.getText();
+				Integer inputDeportes = Integer.parseInt(input_interes_deportivo.getText());
+				Integer inputMusica = Integer.parseInt(input_interes_musical.getText());
+				Integer inputEspectaculos = Integer.parseInt(input_interes_espectaculo.getText());
+				Integer inputCiencia = Integer.parseInt(input_interes_cientifico.getText());
+				app.agregarPersona(inputNombre, inputDeportes, inputMusica, inputEspectaculos, inputCiencia);
+				
+			}
+		});
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(128, 128, 128));
-		panel_1.setBounds(0, 269, 656, 205);
-		contentPane.add(panel_1);
+		btn_ejecutar_algoritmo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					app.armarGrupoPersonas();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+	}
+
+	private void iniciarFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 672, 513);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(64, 128, 128));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 }
